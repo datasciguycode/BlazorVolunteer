158b5d38a08dd2d7b49317e0efb38ea779477333
datasciguycode <146754456+datasciguycode@users.noreply.github.com>
2025-10-26 21:29:02 -0500

    Added security headers

--- Volunteer/Program.cs
+++ Volunteer/Program.cs
@@ -57,6 +57,16 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    // Add security headers
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+        context.Response.Headers.Append("X-Frame-Options", "DENY");
+        context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
+        context.Response.Headers.Append("Referrer-Policy", "strict-origin-when-cross-origin");
+        await next();
+    });
 }
 
 app.UseHttpsRedirection();
